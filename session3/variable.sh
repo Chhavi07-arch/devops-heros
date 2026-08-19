c06a6d24bf8d7007ee25c46b2626bfd0caf165c8
Chhavi07-arch <chhavi.24bcs10201@sst.scaler.com>
2026-08-19 17:49:38 +0530

    Add markdown notes explaining shell scripting concepts

---
+++ session3/variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+variable="Hello, World!"
+echo $variable
+
+#ls="myfile" -- wrong way
+#we can't use commands names as a variable name
+
+name="Chhavi"
+roll_no=10201
+comment="Awesome"
+echo "My name is $name"
+echo "My roll number is $roll_no"
+echo "I am $comment"
